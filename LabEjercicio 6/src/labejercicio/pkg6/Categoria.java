bde604db715d6fe7d660b1904546e2c6edced89b
EmanuelBaigorria <emanueldbaigorria@gmail.com>
2024-04-07 20:03:17 -0300

    ejerciio 6

---
+++ LabEjercicio 6/src/labejercicio/pkg6/Categoria.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package labejercicio.pkg6;
+
+/**
+ *
+ * @author emanu
+ */
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA,
+}
